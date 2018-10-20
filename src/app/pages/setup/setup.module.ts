30ea8b1812186135089e18e219d61f1bbc1abf39
wflixu <wflixu@yeah.net>
2018-10-21 00:31:34 +0800

    init

---
+++ src/app/pages/setup/setup.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SetupComponent } from './setup.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [SetupComponent]
+})
+export class SetupModule { }
